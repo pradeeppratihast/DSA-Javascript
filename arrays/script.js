2d8d30aad9ffa840fdfe1c5ab1b0d126ba231863
pradeeppratihast <pradeep.pratihast016@gmail.com>
2025-07-07 21:32:32 +0530

    Optimize code for insert an element into array

--- arrays/script.js
+++ arrays/script.js
@@ -1,21 +1,26 @@
 let arr = [10, 8, 9, 4, 7, 2];
 
 // PRINT INTO CONSOLE WITH STATIC VALUE
-let new_element = 3;
+/* let new_element = 3;
 let position = 6;
+let i = arr.length - 1;
 position -= 1;
 
 console.log(arr);
-for (let i = arr.length - 1; i >= position; i--) {
-  if (i >= position) {
-    arr[i + 1] = arr[i];
+if (i == position) {
+  arr[i + 1] = new_element;
+} else {
+  for (i; i >= position; i--) {
+    if (i >= position) {
+      arr[i + 1] = arr[i];
+      if (i == position) {
+        arr[i] = new_element;
+      }
+    }
+    console.log(i);
   }
-  if (i == position) {
-    arr[i] = new_element;
-  }
-  console.log(i);
 }
-console.log(arr);
+console.log(arr); */
 
 // INSERT WITH DYNAMIC VALUE
 const dataDisplay = document.getElementById("data_display");
@@ -26,15 +31,18 @@ dataDisplay.innerText = arr;
 
 submit.addEventListener("click", function () {
   const elementValue = parseInt(elementInput.value);
-  let positionValue = parseInt(positionInput.value);
-  positionValue -= 1;
-
-  for (let i = arr.length - 1; i >= positionValue; i--) {
-    if (i >= positionValue) {
-      arr[i + 1] = arr[i];
-    }
-    if (i == positionValue) {
-      arr[i] = elementValue;
+  let positionValue = parseInt(positionInput.value) - 1;
+  let i = arr.length - 1;
+  if (i == positionValue) {
+    arr[i + 1] = elementValue;
+  } else {
+    for (i; i >= positionValue; i--) {
+      if (i >= positionValue) {
+        arr[i + 1] = arr[i];
+        if (i == positionValue) {
+          arr[i] = elementValue;
+        }
+      }
     }
   }
   dataDisplay.innerText = arr;
