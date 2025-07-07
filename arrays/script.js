f0f8c758eafb9422509696599e8bf21150fb83c7
pradeeppratihast <pradeep.pratihast016@gmail.com>
2025-07-07 15:36:07 +0530

    Add code for insert dynamic value into array

--- arrays/script.js
+++ arrays/script.js
@@ -1,4 +1,6 @@
 let arr = [10, 8, 9, 4, 7, 2];
+
+// PRINT INTO CONSOLE WITH STATIC VALUE
 let new_element = 3;
 let position = 6;
 position -= 1;
@@ -14,3 +16,26 @@ for (let i = arr.length - 1; i >= position; i--) {
   console.log(i);
 }
 console.log(arr);
+
+// INSERT WITH DYNAMIC VALUE
+const dataDisplay = document.getElementById("data_display");
+const elementInput = document.getElementById("element");
+const positionInput = document.getElementById("position");
+const submit = document.getElementById("submit");
+dataDisplay.innerText = arr;
+
+submit.addEventListener("click", function () {
+  const elementValue = parseInt(elementInput.value);
+  let positionValue = parseInt(positionInput.value);
+  positionValue -= 1;
+
+  for (let i = arr.length - 1; i >= positionValue; i--) {
+    if (i >= positionValue) {
+      arr[i + 1] = arr[i];
+    }
+    if (i == positionValue) {
+      arr[i] = elementValue;
+    }
+  }
+  dataDisplay.innerText = arr;
+});
