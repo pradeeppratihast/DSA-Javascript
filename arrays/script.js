1d858929334309389f54a4ab39467f1f2d8a2404
pradeeppratihast <pradeep.pratihast016@gmail.com>
2025-07-08 19:10:07 +0530

    Add code for delete element for an array

--- arrays/script.js
+++ arrays/script.js
@@ -2,18 +2,18 @@ let arr = [10, 8, 9, 4, 7, 2];
 
 // PRINT INTO CONSOLE WITH STATIC VALUE
 /* let new_element = 3;
-let position = 6;
+let insert_position = 6;
 let i = arr.length - 1;
-position -= 1;
+insert_position -= 1;
 
 console.log(arr);
-if (i == position) {
+if (i == insert_position) {
   arr[i + 1] = new_element;
 } else {
-  for (i; i >= position; i--) {
-    if (i >= position) {
+  for (i; i >= insert_position; i--) {
+    if (i >= insert_position) {
       arr[i + 1] = arr[i];
-      if (i == position) {
+      if (i == insert_position) {
         arr[i] = new_element;
       }
     }
@@ -23,7 +23,7 @@ if (i == position) {
 console.log(arr); */
 
 // INSERT WITH DYNAMIC VALUE
-const dataDisplay = document.getElementById("data_display");
+/* const dataDisplay = document.getElementById("data_display");
 const elementInput = document.getElementById("element");
 const positionInput = document.getElementById("position");
 const submit = document.getElementById("submit");
@@ -42,6 +42,53 @@ submit.addEventListener("click", function () {
         if (i == positionValue) {
           arr[i] = elementValue;
         }
+      }
+    }
+  }
+  dataDisplay.innerText = arr;
+}); */
+
+// DELETE ELEMENT FROM AN ARRAY STATIC VALUE
+/* let delete_position = 5;
+delete_position -= 1;
+let i = delete_position;
+let j = arr.length - 1;
+
+if (i == j) {
+  arr.length = delete_position;
+} else {
+  for (i; i <= j; i++) {
+    if (i == j) {
+      arr.length = j;
+      break;
+    } else {
+      arr[i] = arr[i + 1];
+    }
+    console.log(i);
+  }
+}
+console.log(arr); */
+
+// DELETE ELEMENT FROM AN ARRAY DYNAMIC VALUE
+const dataDisplay = document.getElementById("data_display");
+const positionInput = document.getElementById("position");
+const submit = document.getElementById("submit");
+dataDisplay.innerText = arr;
+
+submit.addEventListener("click", function () {
+  let positionValue = parseInt(positionInput.value) - 1;
+  let i = positionValue;
+  let j = arr.length - 1;
+
+  if (i == j) {
+    arr.length = positionValue;
+  } else {
+    for (i; i <= j; i++) {
+      if (i == j) {
+        arr.length = j;
+        break;
+      } else {
+        arr[i] = arr[i + 1];
       }
     }
   }
