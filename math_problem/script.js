84bc63376dd06a89f4037b304d64e27ae976b94c
pradeeppratihast <pradeep.pratihast016@gmail.com>
2025-07-10 19:19:30 +0530

    Add code for factorial of a number

--- math_problem/script.js
+++ math_problem/script.js
@@ -1,4 +1,5 @@
 // FIBONACCI SEQUENCE FOR FIRST N ELEMENT
+/*
 function fibonacci(n) {
   const fib = [0, 1];
   for (let i = 2; i < n; i++) {
@@ -10,3 +11,17 @@ function fibonacci(n) {
 fibonacci(2); // [0,1]
 fibonacci(3); // [0,1,1]
 fibonacci(7); // [0,1,1,2,3,5,8]
+*/
+
+// FACTORIAL OF A NUMBER
+function factorial(n) {
+  let fact = 1;
+  for (let i = 2; i <= n; i++) {
+    fact = fact * i;
+  }
+  console.log(fact);
+}
+factorial(0); // 1
+factorial(1); // 1
+factorial(4); // 24
+factorial(5); // 120
