ff4254fe13f37de30a8e78a1fdd40bf90a1317ec
pradeeppratihast <pradeep.pratihast016@gmail.com>
2025-07-10 18:46:48 +0530

    Add code for fibonacci sequence

--- math_problem/script.js
+++ math_problem/script.js
@@ -1,5 +1,5 @@
 // FIBONACCI SEQUENCE FOR FIRST N ELEMENT
-function fibnocci(n) {
+function fibonacci(n) {
   const fib = [0, 1];
   for (let i = 2; i < n; i++) {
     fib[i] = fib[i - 1] + fib[i - 2];
@@ -7,6 +7,6 @@ function fibnocci(n) {
   console.log(fib);
 }
 
-fibnocci(2); // [0,1]
-fibnocci(3); // [0,1,1]
-fibnocci(7); // [0,1,1,2,3,5,8]
+fibonacci(2); // [0,1]
+fibonacci(3); // [0,1,1]
+fibonacci(7); // [0,1,1,2,3,5,8]
