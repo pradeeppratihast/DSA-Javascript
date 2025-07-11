460fcaa89e3b2748c60905c096bf455af2574aa1
pradeeppratihast <pradeep.pratihast016@gmail.com>
2025-07-11 11:10:04 +0530

    Add code for prime number

--- math_problem/script.js
+++ math_problem/script.js
@@ -11,9 +11,11 @@ function fibonacci(n) {
 fibonacci(2); // [0,1]
 fibonacci(3); // [0,1,1]
 fibonacci(7); // [0,1,1,2,3,5,8]
+// Big-O = O(n)
 */
 
 // FACTORIAL OF A NUMBER
+/*
 function factorial(n) {
   let fact = 1;
   for (let i = 2; i <= n; i++) {
@@ -25,3 +27,24 @@ factorial(0); // 1
 factorial(1); // 1
 factorial(4); // 24
 factorial(5); // 120
+// Big-O = O(n)
+*/
+
+// PRIME NUMBER
+function isPrime(n) {
+  if (n < 2) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isPrime(1)); // false
+console.log(isPrime(4)); // false
+console.log(isPrime(5)); // true
+console.log(isPrime(19)); // true
+// Big-O = O(sqrt(n))
